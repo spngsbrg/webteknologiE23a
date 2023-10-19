826be78d744f2d11b7fc8be076cacad440715e18
Thomas Hvid Spangsberg <thsp@eaaa.dk>
2023-10-19 18:40:31 +0200

    Examples for lecture

---
+++ Modul-07/scripts/code-labelling.js
@@ -0,0 +1,11 @@
+let number = 42;
+
+for (let i = 0; i < 10; i++) {}
+
+function sayHello(name, language) {
+  console.log(name + language);
+}
+
+sayHello("John Smith", "Galifreyan");
+
+let headingOne = document.getElementById("heading-one");
